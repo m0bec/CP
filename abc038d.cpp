46ebddc7ab160e82c2dd39f7d82608e541b9962a
mobec <mobec622@outlook.jp>
2018-03-14 22:49:45 +0900

    wa

---
+++ abc038d.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int w[1001], w[1001];
+pair<int, int> s[1001];
+
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> w[i] >> h[i];
+        s[i].first = w[i] + h[i];
+        s[i].second = i;
+    }
+    sort(s, s+n);
+
+    for(int i = n-1; i >= 0; i--){
+        if()
+    }
+}
